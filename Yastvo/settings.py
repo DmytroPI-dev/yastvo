de0637dc140449193790530d38255ba7a3a7dafd
DemetrPI <90642921+DemetrPI@users.noreply.github.com>
2021-11-04 18:16:07 +0300

    edited settings in static dirs

--- Yastvo/settings.py
+++ Yastvo/settings.py
@@ -125,9 +125,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -135,7 +135,7 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/images/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'images/')
+MEDIA_ROOT = os.path.join(BASE_DIR,'images')
 
 
 LOGIN_REDIRECT_URL = 'home'

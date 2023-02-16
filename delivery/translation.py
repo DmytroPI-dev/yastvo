f60d380392b6ce016cae375dfb2f7d1d677c5f87
DemetrPI <90642921+DemetrPI@users.noreply.github.com>
2023-02-16 20:55:58 +0100

    Added translation file

--- delivery/translation.py
+++ delivery/translation.py
@@ -1,9 +1,9 @@
-from modeltranslation.translator import translator, TranslationOptions
 from .models import MenuItems
+from modeltranslation.translator import translator, TranslationOptions
 
 
 class NewTranslationOptions (TranslationOptions):
     fields = ('item', 'item_description', 'item_composition')
 
 
-translator.register(MenuItems, NewTranslationOptions)
+translator.register (MenuItems, NewTranslationOptions)

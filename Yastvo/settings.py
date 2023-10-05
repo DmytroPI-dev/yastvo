14a750fb185972068652f9abaf10e1df934f87fe
DemetrPI <90642921+DemetrPI@users.noreply.github.com>
2023-10-05 22:34:19 +0200

    Updated allowed hosts

--- Yastvo/settings.py
+++ Yastvo/settings.py
@@ -44,7 +44,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1:8000','yastvo.azurewebsites.net/']
+ALLOWED_HOSTS = ['127.0.0.1:8000','yastvo.azurewebsites.net']
 
 # Google Cloud Storage Configurations
 GS_BUCKET_NAME = 'yastvo_bucket'
